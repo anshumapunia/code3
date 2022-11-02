7d4fe0793f71a5972ded1a7042f3d99137995a9f
anshumapunia123 <112813318+anshumapunia123@users.noreply.github.com>
2022-11-02 23:05:01 +0530

    primeno

---
+++ primeno.js
@@ -0,0 +1,17 @@
+    let num=17;
+    let count=0;
+for(let i=1;i<=num;i++)
+  {
+    if(num%i==0)
+    {
+      count++;
+    }
+  }
+if(count==2)
+{
+  console.log("Prime no");
+}
+else 
+{
+  console.log("Not Prime no")
+}
